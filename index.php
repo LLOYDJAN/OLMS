5b9c47ca8153c1943bbf3d199a72b7ab3b511ba4
LLOYDJAN <48742183+LLOYDJAN@users.noreply.github.com>
2019-03-25 08:34:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include ('theme/header.php');  ?>
+<?php include ('theme/sidebar.php'); ?>
+<?php include ('content.php'); ?>
+<?php include ('theme/footer.php'); ?>
---
+++ user_sidebar.php
@@ -0,0 +1,54 @@
+    <div id="wrapper">
+
+      <!-- Sidebar -->
+      <ul class="sidebar navbar-nav">
+      
+        <li class="nav-item active">
+          <a class="nav-link" href="dashboard.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+          </a>
+        </li>
+          <li class="nav-item active">
+          <a class="nav-link" href="My-profile.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>My Profile</span>
+          </a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="issued-books.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Issued Book(s)</span>
+          </a>
+        </li>
+       
+        <li class="nav-item">
+          <a class="nav-link" href="book.php">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>BOOKS</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="authors.php">
+            <i class="fas fa-fw fa-table"></i>
+            <span>AUTHORS</span></a>
+        </li>
+        
+
+         <li class="nav-item">
+          <a class="nav-link" href="studborrow.php">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Borrow Book(s) </span></a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">
+            <i class="fas fa-fw fa-table"></i>
+            <span>LOG OUT</span></a>
+        </li>
+
+        
+      </ul>
+
+       <div id="content-wrapper">
+
+        <div class="container-fluid">
